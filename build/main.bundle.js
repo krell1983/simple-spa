1bd9ffec73825701487c4f355772e430be16f532
Piotr Markiewicz <pmarkeiwicz@getresponse.com>
2017-12-11 09:44:03 +0100

    Test

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -70,37 +70,65 @@
 "use strict";
 
 
-var userData = '';
-var domLink = document.querySelectorAll('[data-link]');
-var domChange = document.querySelectorAll('[data-change]');
+var globalState = {
+    user: {
+        userFirstName: 'name1',
+        userSecondName: 'secondName',
+        userNumber: 1,
+        testArray: [0, 1, 2, 4, 5],
+        testObject: {
+            test1: 1,
+            test2: 2,
+            test3: 3
+        }
+    },
+    db: {
+        t1: 1,
+        t2: 2,
+        t3: 3,
+        t4: 4,
+        t5: 5
+    }
+};
 
-function domRender(variable, newData) {
-    console.log(newData);
+function initSimpleSPA(state) {
+    var domLink = document.querySelectorAll('[data-link]');
+    var domChange = document.querySelectorAll('[data-change]');
+
     domLink.forEach(function (domItem) {
-        if (domItem.dataset.link === variable) {
-            domItem.innerHTML = newData;
+        var selector = domItem.dataset.link;
+        if (selector && !state || state === selector) {
+            domItem.innerHTML = eval(selector);
         }
     });
 
     domChange.forEach(function (domItem) {
-        if (domItem.dataset.change === variable) {
-            domItem.value = newData;
+        var selector = domItem.dataset.change;
+        if (selector && !state || state === selector) {
+            domItem.value = eval(selector);
         }
     });
-}
 
-function domAddEventListener() {
     domChange.forEach(function (domItem) {
-        domItem.addEventListener("change", onChange);
+        domItem.addEventListener("change", onSimpleSPAChange);
     });
 }
 
-function onChange(e) {
-    domRender(e.srcElement.dataset.change, e.srcElement.value.replace(/ +[^a-zA-Z0-9]/g, ""));
+function onSimpleSPAChange(e) {
+    globalStateChange(e.srcElement.dataset.change, e.srcElement.value.replace(/[^a-zA-Z0-9 ]/g, ""));
 }
 
-domRender('userData', userData);
-domAddEventListener('userData');
+function globalStateChange(state, value) {
+    console.log('%c[Global State Change]:' + '%c ' + state + ':%c' + value, 'background: #1e5adb; color:#fff; padding:2px;', 'font-weight: bold;', 'font-weight: bold; color:#1e5adb;');
+    eval(state + ' = "' + value + '"');
+    initSimpleSPA(state, value);
+}
+
+initSimpleSPA();
+globalStateChange('globalState.user.userNumber', 10);
+
+//globalStateChange('globalState.user.userName', 'newName');
+//console.log(globalState);
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,31 +1,59 @@
-let userData = '';
-let domLink = document.querySelectorAll('[data-link]');
-let domChange = document.querySelectorAll('[data-change]');
+let globalState = {
+    user: {
+        userFirstName: 'name1',
+        userSecondName: 'secondName',
+        userNumber: 1,
+        testArray: [0, 1, 2, 4, 5],
+        testObject: {
+            test1: 1,
+            test2: 2,
+            test3: 3,
+        }
+    },
+    db: {
+        t1: 1,
+        t2: 2,
+        t3: 3,
+        t4: 4,
+        t5: 5,
+    }
+};
 
-function domRender(variable, newData) {
-    console.log(newData);
+function initSimpleSPA(state) {
+    const domLink = document.querySelectorAll('[data-link]');
+    const domChange = document.querySelectorAll('[data-change]');
+
     domLink.forEach(domItem => {
-        if (domItem.dataset.link === variable) {
-            domItem.innerHTML = newData;
+        const selector = domItem.dataset.link;
+        if (selector && !state || state === selector) {
+            domItem.innerHTML = eval(selector);
         }
     });
 
     domChange.forEach(domItem => {
-        if (domItem.dataset.change === variable) {
-            domItem.value = newData;
+        const selector = domItem.dataset.change;
+        if (selector && !state || state === selector) {
+            domItem.value = eval(selector);
         }
     });
-}
 
-function domAddEventListener() {
     domChange.forEach(domItem => {
-        domItem.addEventListener("change", onChange);
+        domItem.addEventListener("change", onSimpleSPAChange);
     });
 }
 
-function onChange (e) {
-    domRender(e.srcElement.dataset.change, e.srcElement.value.replace(/ +[^a-zA-Z0-9]/g, ""));
+function onSimpleSPAChange(e) {
+    globalStateChange(e.srcElement.dataset.change, e.srcElement.value.replace(/[^a-zA-Z0-9 ]/g, ""));
 }
 
-domRender('userData', userData);
-domAddEventListener('userData');
+function globalStateChange(state, value) {
+    console.log('%c[Global State Change]:'+ '%c ' +state+':%c' + value, 'background: #1e5adb; color:#fff; padding:2px;', 'font-weight: bold;', 'font-weight: bold; color:#1e5adb;');
+    eval(`${state} = "${value}"`);
+    initSimpleSPA(state, value);
+}
+
+initSimpleSPA();
+globalStateChange('globalState.user.userNumber', 10);
+
+//globalStateChange('globalState.user.userName', 'newName');
+//console.log(globalState);
